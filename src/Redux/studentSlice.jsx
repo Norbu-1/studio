44cec776d3dfa9c2ff5819fa398ddfa4749e15ad
Tsewang Norbu <153228252+Norbu-1@users.noreply.github.com>
2024-12-23 14:06:54 +0530

    Update studentSlice.jsx

--- src/Redux/studentSlice.jsx
+++ src/Redux/studentSlice.jsx
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/students';
+const API_URL = 'https://studio-7.onrender.com/students';
 
 // Fetch students
 export const fetchStudents = createAsyncThunk('students/fetchStudents', async () => {
